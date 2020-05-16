28f8f493ea6859d23e15126439cc70568b2dd1d5
Joseph Manley <j@cloudsumu.com>
2020-05-16 17:07:13 -0400

    Enable progression

--- client/Scripts/Entities/NPCs/IntroScientist.gd
+++ client/Scripts/Entities/NPCs/IntroScientist.gd
@@ -4,7 +4,7 @@ func _ready():
 	connect("interacted", self, "_on_interact")
 	$Speaker.speaker = "fast_talker"
 	$Speaker.speaker_name = "Dr.Thadd"
-	#$Speaker.start_dialog("intro_science")
+	$Speaker.start_dialog("intro_science")
 
 func _on_interact():
 	$Speaker.start_dialog("intro_science_followup")
