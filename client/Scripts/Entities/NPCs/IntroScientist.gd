afde971c17e3d407530f9f3cb0a26c058545333e
Joseph Manley <j@cloudsumu.com>
2020-05-16 11:31:16 -0400

    Misc bugfixes

--- client/Scripts/Entities/NPCs/IntroScientist.gd
+++ client/Scripts/Entities/NPCs/IntroScientist.gd
@@ -3,7 +3,7 @@ extends "res://Scripts/Component/Interactable.gd"
 func _ready():
 	connect("interacted", self, "_on_interact")
 	$Speaker.speaker_name = "Dr.Thadd"
-	#$Speaker.start_dialog("intro_science")
+	$Speaker.start_dialog("intro_science")
 
 func _on_interact():
 	$Speaker.start_dialog("intro_science_followup")
